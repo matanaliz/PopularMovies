16f71b402f854b4e8dfc8e4ebfc4f5250ed67af2
matanaliz <ol.kaspruk@gmail.com>
2016-06-08 01:55:40 +0300

    Added JSON parser, asynk task and movie object.

---
+++ app/src/main/java/mtn/popularmovies/FetchMoviesAsyncTask.java
@@ -0,0 +1,92 @@
+package mtn.popularmovies;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by matan on 08.06.2016.
+ */
+public class FetchMoviesAsyncTask extends AsyncTask<String, Void, ArrayList<Movie> > {
+
+    private final String LOG_TAG = FetchMoviesAsyncTask.class.getSimpleName();
+
+    private final String API_KEY = BuildConfig.THE_MOVIE_DB_API_KEY;
+
+    private final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+    private final String POPULAR_BASE_URL = "http://api.themoviedb.org/3/movie/popular?";
+    private final String TOP_RATED_BASE_URL = "http://api.themoviedb.org/3/movie/top_rated?";
+
+    @Override
+    protected ArrayList<Movie> doInBackground(String... location) {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String responseJsonStr = null;
+
+        try {
+
+            final String API_KEY_PARAM = "api_key";
+            Uri builtUri = Uri.parse(POPULAR_BASE_URL).buildUpon()
+                    .appendQueryParameter(API_KEY_PARAM, API_KEY).build();
+
+            URL url = new URL(builtUri.toString());
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                return null;
+            }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ( (line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                return null;
+            }
+
+            responseJsonStr = buffer.toString();
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                }
+            }
+        }
+
+        return new MovieParser().parseResponse(responseJsonStr);
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<Movie> result) {
+        if (result != null) {
+            for (Movie item : result) {
+                Log.d(LOG_TAG, item.getOriginalTitle());
+            }
+        }
+    }
+}
---
+++ app/src/main/java/mtn/popularmovies/MovieParser.java
@@ -0,0 +1,62 @@
+package mtn.popularmovies;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by matan on 08.06.2016.
+ */
+public class MovieParser {
+    private final String LOG_TAG = MovieParser.class.getSimpleName();
+
+    public ArrayList<Movie> parseResponse(String response) {
+
+        if (null == response) return null;
+
+        ArrayList<Movie> movieList = new ArrayList<>();
+
+        try {
+            final String MOVIE_ARRAY = "results";
+            final String MOVIE_ID = "id";
+            final String MOVIE_POSTER_URL = "poster_path";
+            final String MOVIE_OVERVIEW = "overview";
+            final String MOVIE_RELEASE_DATE = "release_date";
+            final String MOVIE_TITLE = "title";
+            final String MOVIE_ORIGINAL_TITLE = "original_title";
+            final String MOVIE_VOTE_RATING = "vote_average";
+
+            JSONObject res = new JSONObject(response);
+            JSONArray jMovies = res.getJSONArray(MOVIE_ARRAY);
+
+            for (int i=0; i < jMovies.length(); i++)
+            {
+                try {
+                    JSONObject jMovie = jMovies.getJSONObject(i);
+
+                    movieList.add(new Movie()
+                            .setId(jMovie.getInt(MOVIE_ID))
+                            .setOriginalTitle(jMovie.getString(MOVIE_ORIGINAL_TITLE))
+                            .setTitle(jMovie.getString(MOVIE_TITLE))
+                            .setOverview(jMovie.getString(MOVIE_OVERVIEW))
+                            .setPosterUrl(jMovie.getString(MOVIE_POSTER_URL))
+                            .setReleaseDate(jMovie.getString(MOVIE_RELEASE_DATE))
+                            .setVoteRating(jMovie.getDouble(MOVIE_VOTE_RATING))
+                    );
+
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                }
+            }
+
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+
+        return movieList;
+    }
+}
