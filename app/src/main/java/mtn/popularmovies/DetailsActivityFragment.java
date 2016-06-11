04f0242cb2cf403537baa7931ca9e1067a72d4c3
matanaliz <ol.kaspruk@gmail.com>
2016-06-11 17:10:37 +0300

    Added more details to DetailsFragment.

--- app/src/main/java/mtn/popularmovies/DetailsActivityFragment.java
+++ app/src/main/java/mtn/popularmovies/DetailsActivityFragment.java
@@ -31,10 +31,17 @@ public class DetailsActivityFragment extends Fragment {
         if (null != intent && intent.hasExtra(getString(R.string.details_intent))) {
             mMovie = intent.getParcelableExtra(getString(R.string.details_intent));
 
+            TextView title = (TextView) rootView.findViewById(R.id.details_title);
             TextView overview = (TextView) rootView.findViewById(R.id.details_overview);
+            TextView releaseDate = (TextView) rootView.findViewById(R.id.details_release_date);
+            TextView voteRate = (TextView) rootView.findViewById(R.id.details_vote_rate);
             ImageView image = (ImageView) rootView.findViewById(R.id.details_poster);
 
+            title.setText(mMovie.getTitle());
             overview.setText(mMovie.getOverview());
+            releaseDate.setText(mMovie.getReleaseDate());
+            voteRate.setText(Double.toString(mMovie.getVoteRating()));
+
             Picasso.with(getActivity()).load("http://image.tmdb.org/t/p/w342/" + mMovie.getPosterUrl())
                     .into(image);
 
