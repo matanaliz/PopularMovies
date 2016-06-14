55ba9f73dfcf4834cc6d18056aeaf23f8aeb695e
matanaliz <ol.kaspruk@gmail.com>
2016-06-14 23:57:55 +0300

    Removed unused imports

--- app/src/main/java/mtn/popularmovies/MainActivity.java
+++ app/src/main/java/mtn/popularmovies/MainActivity.java
@@ -2,8 +2,6 @@ package mtn.popularmovies;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
