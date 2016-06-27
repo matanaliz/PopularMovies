2e13a7df47c9a2a1e6c802c063dc43f94e20e60c
Oleksandr Kaspruk <ol.kaspruk@gmail.com>
2016-06-27 17:59:29 +0300

    Added ViewHolder, moved padding values to dimens.xml.

--- app/src/main/java/mtn/popularmovies/MovieArrayAdapter.java
+++ app/src/main/java/mtn/popularmovies/MovieArrayAdapter.java
@@ -17,6 +17,14 @@ import java.util.List;
  */
 public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
+    private static class ViewHolder {
+        public ImageView mImageView;
+
+        public ViewHolder(ImageView image) {
+            this.mImageView = image;
+        }
+    }
+
     private final String LOG_TAG = MovieArrayAdapter.class.getSimpleName();
 
     private Context mContext;
@@ -28,15 +36,23 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
     public View getView(int position, View convertView, ViewGroup parent)
     {
+        ImageView posterImage;
         Movie movie = getItem(position);
 
         if (null == convertView) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.poster_item, parent, false);
+            posterImage = (ImageView) convertView.findViewById(R.id.poster_view);
+            convertView.setTag(new ViewHolder(posterImage));
+        } else {
+            ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+            posterImage = viewHolder.mImageView;
         }
 
-        ImageView posterImage = (ImageView) convertView.findViewById(R.id.poster_view);
-
-        Picasso.with(mContext).load("http://image.tmdb.org/t/p/w500/" + movie.getPosterUrl())
+        // TODO check if url was well formed
+        Picasso.with(mContext)
+                .load("http://image.tmdb.org/t/p/w325/" + movie.getPosterUrl())
+                .placeholder(R.drawable.image_placeholder)
+                .error(R.drawable.image_error_placeholder)
                 .into(posterImage);
 
         return convertView;
